4d435d4490551d84cb752470a22cc576f32b73b1
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-22 12:23:27 -0700

    feat: Setup db and add response headers

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,30 @@
 from flask import Flask
 from flask_cors import CORS
+from models import setup_db
 
 app = Flask(__name__)
+setup_db(app)
 CORS(app)
+
+
+@app.after_request
+def after_request(response):
+    """Adds response headers after request.
+
+    Args:
+        response: The response object to add headers to
+
+    Returns:
+        response: The response object that the headers were added to
+    """
+    response.headers.add(
+        "Access-Control-Allow-Headers", "Content-Type, Authorization, true"
+    )
+    response.headers.add(
+        "Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"
+    )
+
+    return response
 
 
 if __name__ == "__main__":
